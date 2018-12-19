47c80fcbfca401f39aafa1f2dd912e43bd114d60
Eloy Vega <eloy.vega@wizeline.com>
2018-12-18 20:07:26 -0600

    Ponche y tamales

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hola
+            Demo en vivo en la posada, con ponche y tamales.
           </p>
           <a
             className="App-link"
