22d0b70ba63ddc68cb7718a9d49793efa351776a
Eloy Vega <eloy.vega@wizeline.com>
2018-12-18 20:03:20 -0600

    hola

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Posada AWS!
+            Hola
           </p>
           <a
             className="App-link"
